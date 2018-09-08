2d32f92451472796f2c74b83f46066961661a281
Aaron Burk <aarondburk@gmail.com>
2018-09-08 17:08:21 -0500

    initial bootstrapping of notes app with a simple note model

---
+++ src/main/java/com/example/notater/NotaterApplication.java
@@ -0,0 +1,12 @@
+package com.example.notater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaterApplication.class, args);
+	}
+}
